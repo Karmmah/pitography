11fa7e2d437e8e93c094c30e082f36e5a71de57a
Karmmah <kramer.patrick@protonmail.com>
2023-01-23 14:03:46 +0100

    restructuring

--- simple_capture.py
+++ simple_capture.py
@@ -18,10 +18,24 @@ Lcd_ScanDir = LCD_1in44.SCAN_DIR_DFT
 disp.LCD_Init(Lcd_ScanDir)
 disp.LCD_Clear()
 
-# draw initial image
+# ui parameters
 ui_width, ui_height = 128, 128
-ui = Image.new("RGB", (ui_width,ui_height))
-draw = ImageDraw.Draw(ui)
+#(4056,3040) #max resolution, gpu_mem in /boot/config.txt needs to be changed to 256 to have enough resources for this resolution
+#capture_resolution = (4056,3040)
+capture_resolution = (4032,3040)
+preview_resolution = (ui_width,ui_height)
+
+# create startup image
+startup_image = Image.new("RGB", (ui_width,ui_height))
+startup_image_draw = ImageDraw.Draw(startup_image)
+startup_image_draw.rectangle( (50,50,ui_width-50,ui_height-50), fill=0x00ff00)
+
+# create capture success screen
+capture_success = Image.new("RGB", (ui_width,ui_height))
+capture_success_draw = ImageDraw.Draw(capture_success)
+capture_success_draw.rectangle( (0,0,ui_width,ui_height), fill=0 )
+capture_success_draw.text( (0,0), "Image saved" )
+capture_success = capture_success.rotate(180)
 
 # GPIO init
 GPIO.setmode(GPIO.BCM)
@@ -29,23 +43,7 @@ GPIO.setup(shutter_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(backlight_pin, GPIO.OUT, initial=1)
 GPIO.setup(magnify_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-def main(ui, draw, cam):
-	draw.rectangle( (50,50,ui_width-50,ui_height-50), fill=0x00ff00)
-	disp.LCD_ShowImage(ui, 0, 0)
-
-#	(4056,3040) #max resolution, gpu_mem in /boot/config.txt needs to be changed to 256 to have enough resources for this resolution
-#	capture_resolution = (4056,3040)
-	capture_resolution = (4032,3040)
-	preview_resolution = (ui_width,ui_height)
-
-	# fit preview to what will actually be captured at a given resolution
-#	if capture_resolution[1] <= capture_resolution[0]: #landscape orientation
-#		preview_zoom = ((1-capture_resolution[0]/4056)/2, (1-capture_resolution[1]/3040)/2, capture_resolution[1]/3040, capture_resolution[1]/3040)
-#		preview_zoom = (0,0,1,1)
-#	else: #portrait orientation
-#		preview_zoom = ((1-3040/4056)/2+3040/4056*(capture_resolution[0]/3040)/2, 0, capture_resolution[0]/3040, capture_resolution[0]/3040)
-#	print("preview_zoom", preview_zoom) #debug
-
+def main(cam):
 	magnify_zoom = (0.35,0.35,0.3,0.3)
 
 	min_shutter_speed = 20000 #minimal shutter speed 1/50 (20000 µs)
@@ -66,7 +64,7 @@ def main(ui, draw, cam):
 			# blank the backlight to visualise that the image is being taken
 			GPIO.output(backlight_pin, 0)
 
-			# set the capture resolution and reset crop
+			# set the capture resolution and reset crop, rotation and magnification
 			cam.resolution = capture_resolution
 			cam.rotation = 0
 			magnify_flag = False
@@ -74,14 +72,12 @@ def main(ui, draw, cam):
 
 			# capture the image
 #			cam.capture( "/home/pi/DCIM/%d.jpg" % int(time.time()*1000), format="raw" )
+#			cam.capture( "/home/pi/DCIM/%d.jpg" % int(time.time()*1000), format=".jpg", bayer=True )
 			cam.capture( "/home/pi/DCIM/%d.jpg" % int(time.time()*1000) )
 			print("image captured:", int(time.time()*1000))
 
 			# display capture success message
-			draw.rectangle( (0,0,ui_width,ui_height), fill=0 )
-			draw.text( (0,0), "Image saved" )
-#			draw.rotate(180)
-			disp.LCD_ShowImage(ui, 0, 0)
+			disp.LCD_ShowImage(capture_success, 0, 0)
 
 			# reset resolution to preview
 			cam.resolution = preview_resolution
@@ -92,8 +88,6 @@ def main(ui, draw, cam):
 
 		# show preview image on screen
 		else:
-#			cam.zoom = preview_zoom
-
 			#preview variation 1 with Bytestream
 #			stream = BytesIO()
 #			cam.capture(stream, format="jpeg")
@@ -119,11 +113,8 @@ def main(ui, draw, cam):
 #			else:
 #				ov_draw.text( (30,100), text="fs not running", fill=0xff0000 )
 
+			# check if internet connection is available and displey the cameras ip address
 			ov_draw.text( (25,115), text=subprocess.check_output("hostname -I", text=True, shell=True)[:13], fill=0xffffff)
-
-			# add current camera info to preview
-			#properties of camera are saved as Fraction objects; need special handling
-			gain = cam.analog_gain.numerator / cam.analog_gain.denominator
 
 #from the pi camera guide
 #camera.brightness = 50 (0 to 100)
@@ -138,7 +129,12 @@ def main(ui, draw, cam):
 #camera.rotation = 0
 #camera.crop = (0.0, 0.0, 1.0, 1.0)
 
-			ov_draw.text( (3,20), "g "+str(round(gain,1)), fill=0xffffff )
+			# add current camera info to preview
+			#properties of camera are saved as Fraction objects; need special handling
+			ag = cam.analog_gain.numerator / cam.analog_gain.denominator
+			dg = cam.digital_gain.numerator / cam.digital_gain.denominator
+			ov_draw.text( (3,10), "ag "+str(round(ag,1)), fill=0xffffff )
+			ov_draw.text( (3,20), "dg "+str(round(dg,1)), fill=0xffffff )
 			ov_draw.text( (3,30), "e 1/"+str(int(1000000/cam.exposure_speed)), fill=0xffffff )
 			ov_draw.text( (3,40), "s 1/"+(str(int(1000000/cam.shutter_speed)) if cam.shutter_speed > 0 else "0"), fill=0xffffff )
 			ov_draw.text( (3,50), "i "+(str(cam.iso) if cam.iso != 0 else "auto"), fill=0xffffff )
@@ -160,15 +156,14 @@ def main(ui, draw, cam):
 
 if __name__ == "__main__":
 	try:
-		draw.rectangle( (50,50,ui_width-50,ui_height-50), fill=0x00ff00)
-		disp.LCD_ShowImage(ui, 0, 0)
+		disp.LCD_ShowImage(startup_image, 0, 0)
 
 		cam = pc.PiCamera(framerate=24)
 #		time.sleep(2)
 		cam.zoom = (0,0,1,1) #reset camera crop
-		cam.start_preview() #preview to adjust exposure to available light
+		cam.start_preview() #preview to let the camera adjust exposure to available light
 
-		main(ui, draw, cam)
+		main(cam)
 
 #	except:
 #		print("ERROR")
