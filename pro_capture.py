a175664c7282675449bdc634deef3352cd4c86fe
Karmmah <kramer.patrick@protonmail.com>
2023-03-13 00:22:53 +0100

    added first text to main menu and added shooting modes

--- pro_capture.py
+++ pro_capture.py
@@ -24,15 +24,15 @@ import picamera
 #RST 			P27 		Reset
 #BL 			P24		Backlight
 
-def main():
-	ui_width, ui_height = 128, 128
-	button_press_wait_time = 0.3
+# button mapping
+shutter_pin = 13
+backlight_pin = 24
+magnify_pin = 16
+menu_pin = 21
 
-	# button mapping
-	shutter_pin = 13
-	backlight_pin = 24
-	magnify_pin = 16
-	menu_pin = 21
+def main(cam):
+	ui_width, ui_height = 128, 128
+	button_press_wait_time = 0.2
 
 	# GPIO init
 	GPIO.setmode(GPIO.BCM)
@@ -62,7 +62,6 @@ def main():
 	magnify_flag = False
 
 	# set up camera
-	cam = picamera.PiCamera(framerate=24)
 	cam.resolution = preview_resolution
 	cam.rotation = 180 #rotation for preview only
 
@@ -77,11 +76,37 @@ def main():
 	main_menu_screen = Image.new("RGB", (ui_width,ui_height))
 	main_menu_screen_draw = ImageDraw.Draw(main_menu_screen)
 	main_menu_screen_draw.rectangle( (0,0,ui_width,ui_height), fill= 0xffab32)
-	menu_flag = False
-	menu_index = 0 #0:main menu, 1: timelapse , 11:timelapse interval settings
+	main_menu_screen_draw.polygon( (59,58,69,58,64,53), fill=0x000000)
+	main_menu_screen_draw.polygon( (70,59,70,69,75,64), fill=0x000000)
+	main_menu_screen_draw.polygon( (59,70,69,70,64,75), fill=0x000000)
+	main_menu_screen_draw.polygon( (58,59,58,69,53,64), fill=0x000000)
+	main_menu_screen_draw.text((49,30), "Photo")
+	main_menu_screen_draw.text((83,58), "TMLPSE")
+	main_menu_screen = main_menu_screen.rotate(180)
+	menu_index = 0 #0: no menu, 1: main menu, 11: still photo settings, 2: timelapse , 21:timelapse interval settings
+
+	capture_mode = 0 #0: still photo, 1: timelapse
+	timelapse_capture_flag = False
+	timelapse_interval = 5 #[s]
+	last_timelapse_frame_time = 0
 
 	# main loop
 	while True:
+		# show menu
+		if GPIO.input(menu_pin) == 0 and not timelapse_capture_flag:
+			menu_index = 1 if menu_index == 0 else 0
+			time.sleep(button_press_wait_time)
+		if menu_index != 0:
+			if menu_index == 1: #main menu
+				print("main menu with colors")
+#				active_color, inactive_color = 0x00ff00, 0xffab32
+				active_color, inactive_color = 0x00ff00, 0xff0000
+#				main_menu_screen_draw.line((53,37,75,37), fill=active_color if capture_mode == 0 else inactive_color, width=5)
+				main_menu_screen_draw.rectangle((9,7,75,79), fill=0xffffff)
+#				main_menu_screen_draw.line((86,65,98,65), fill=active_color if capture_mode == 1 else inactive_color, width=5)
+			disp.LCD_ShowImage(main_menu_screen, 0, 0)
+			continue #skip capturing and preview while in menu
+
 		# change magnification
 		if GPIO.input(magnify_pin) == 0:
 			magnify_flag = not magnify_flag
@@ -91,8 +116,23 @@ def main():
 				cam.zoom = (0,0,1,1)
 			time.sleep(button_press_wait_time)
 
-		# capture image
-		if GPIO.input(shutter_pin) == 0:
+		# capture timelapse
+		if GPIO.input(shutter_pin) == 0 and capture_mode == 1:
+			timelapse_capture_flag = not timelapse_capture_flag
+			time.sleep(button_press_wait_time)
+		if timelapse_capture_flag:
+			if time.time() > last_timelapse_frame_time + timelapse_interval:
+				GPIO.output(backlight_pin, 0)
+				cam.zoom = (0,0,1,1)
+				cam.rotation = 0
+				cam.resolution = capture_resolution
+				cam.capture("/home/pi/DCIM/timelapse/%d.jpg" & int(time.time()*1000), use_video_port=False)
+				cam.resolution = preview_resolution
+				cam.rotation = 180
+				GPIO.output(backlight_pin, 1)
+
+		# capture still image
+		if GPIO.input(shutter_pin) == 0 and capture_mode == 0:
 			GPIO.output(backlight_pin, 0) #blank backlight to show image was taken
 			# set camera to capture settings
 			cam.zoom = (0,0,1,1)
@@ -109,14 +149,6 @@ def main():
 			cam.rotation = 180
 			GPIO.output(backlight_pin, 1)
 
-		# show menu
-		if GPIO.input(menu_pin) == 0:
-			menu_flag = not menu_flag
-			time.sleep(button_press_wait_time)
-		if menu_flag:
-			disp.LCD_ShowImage(main_menu_screen, 0, 0)
-			continue
-
 		# update preview
 		overlay = Image.new("L", (ui_width,ui_height))
 		ov_draw = ImageDraw.Draw(overlay)
@@ -131,7 +163,7 @@ def main():
 		ag = cam.analog_gain.numerator / cam.analog_gain.denominator
 		dg = cam.digital_gain.numerator / cam.digital_gain.denominator
 		s = str(int(1000000/cam.exposure_speed)) if cam.exposure_speed < max_shutter_time else str(int(1000000/max_shutter_time))
-		ov_draw.text( (50,3), "pro", fill=0xffffff )
+		ov_draw.text( (54,3), "pro", fill=0xffffff )
 		ov_draw.text( (3,10), "ag "+str(round(ag,1)), fill=0xffffff )
 		ov_draw.text( (3,20), "dg "+str(round(dg,1)), fill=0xffffff )
 		ov_draw.text( (3,30), "e 1/"+str(int(1000000/cam.exposure_speed)), fill=0xffffff )
@@ -144,17 +176,16 @@ def main():
 		preview.paste(ImageOps.colorize(overlay, (0,0,0), (255,255,255)), (0,0), overlay)
 		disp.LCD_ShowImage(preview, 0, 0)
 
-	# clean up everything and close everything
-	cam.close()
-	print("Closed camera")
-	GPIO.output(backlight_pin, 0)
-	print("Disabled backlight")
-	GPIO.cleanup()
-	print("GPIO cleanup")
-
 if __name__ == "__main__":
 	try:
-		main()
+		cam = picamera.PiCamera(framerate=24)
+		main(cam)
 	finally:
+		cam.close()
+		print("\nClosed camera")
+		GPIO.output(backlight_pin, 0)
+		print("Disabled backlight")
+		GPIO.cleanup()
+		print("GPIO cleanup")
 		print("\nPro Capture closed")
 
