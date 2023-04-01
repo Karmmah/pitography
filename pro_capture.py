9a39fc39fa03f29c1541ccd057c9d5480d972f82
Karmmah <kramer.patrick@protonmail.com>
2023-04-01 17:44:53 +0200

    added exposure mode selection

--- pro_capture.py
+++ pro_capture.py
@@ -62,15 +62,16 @@ def main(cam, disp):
 	main_menu_index = 1
 	main_menu_screen = Image.new("RGB", (ui_width,ui_height))
 	main_menu_screen_draw = ImageDraw.Draw(main_menu_screen)
-	main_menu_screen_draw.rectangle( (0,0,ui_width,ui_height), fill= 0xd89552)
+	main_menu_screen_draw.rectangle( (0,0,ui_width,ui_height), fill=0xffffff)
 	#arrows
 	main_menu_screen_draw.polygon( (59,58,69,58,64,53), fill=0x000000)
 	main_menu_screen_draw.polygon( (70,59,70,69,75,64), fill=0x000000)
 	main_menu_screen_draw.polygon( (59,70,69,70,64,75), fill=0x000000)
 	main_menu_screen_draw.polygon( (58,59,58,69,53,64), fill=0x000000)
 	#labels
-	main_menu_screen_draw.text((11,58), " Photo")
-	main_menu_screen_draw.text((32,30), " Timelapse")
+	main_menu_screen_draw.text((11,58), " Photo", fill=0x000000)
+	main_menu_screen_draw.text((32,30), " Timelapse", fill=0x000000)
+	main_menu_screen_draw.text((32,86), " Settings", fill=0x000000)
 	main_menu_screen = main_menu_screen.rotate(180)
 
 	# still menu setup
@@ -95,17 +96,31 @@ def main(cam, disp):
 	timelapse_menu_draw.text((21,30), " Interval")
 	timelapse_menu_draw.text((9,47), " Resolution")
 
+	# settings menu setup
+	settings_menu_index = 4
+	settings_menu_screen = Image.new("RGB", (ui_width,ui_height))
+	settings_menu_draw = ImageDraw.Draw(settings_menu_screen)
+	settings_menu_draw.rectangle((0,0,ui_width,ui_height), fill=0xd89552)
+	settings_menu_draw.text((25,8), " Settings")
+	settings_menu_draw.text((4,30), " Exp. Mode")
+
 	current_capture_mode = 0
 	still_capture_index = 0
 	timelapse_capture_index = 1
+
 	timelapse_capture_flag = False
 	last_timelapse_frame_time = 0
 	last_timelapse_exposure_times = [] #save exposure times of the last photos taken to smooth out exposure
+
+	exposure_modes = ['off', 'auto', 'night', 'nightpreview', 'backlight', 'spotlight', 'sports', 'snow', 'beach',
+'verylong', 'fixedfps', 'antishake', 'fireworks']
+	exposure_mode_index = 1 #default: auto
 
 	# reassign draw objects to menu screens after rotation (otherwise it cannot be drawn to again)
 	main_menu_screen_draw = ImageDraw.Draw(main_menu_screen)
 #	still_menu_draw = ImageDraw.Draw(still_menu_screen)
 	timelapse_menu_draw = ImageDraw.Draw(timelapse_menu_screen)
+	settings_menu_draw = ImageDraw.Draw(settings_menu_screen)
 
 	last_interaction_time = time.time()
 
@@ -144,7 +159,7 @@ def main(cam, disp):
 			#main menu
 			if current_menu_index == main_menu_index:
 #				active_color, inactive_color = 0x0000ff, 0xffab32
-				active_color, inactive_color = 0x0000ff, 0xd89552
+				active_color, inactive_color = 0x0000ff, 0xffffff
 #				active_color, inactive_color = 0x00ff00, 0x0000ff
 				main_menu_screen_draw.line((106,56,88,56), fill = active_color if current_capture_mode == still_capture_index else inactive_color, width=3)
 				main_menu_screen_draw.line((46,83,82,83), fill = active_color if current_capture_mode == timelapse_capture_index else inactive_color, width=3)
@@ -156,6 +171,9 @@ def main(cam, disp):
 				elif input_key == left_pin:
 					current_capture_mode = still_capture_index
 					current_menu_index = still_menu_index
+				elif input_key == down_pin:
+					current_menu_index = settings_menu_index
+#				print("current menu index:", current_menu_index) #debug
 
 			#still photo menu
 			elif current_menu_index == still_menu_index:
@@ -190,6 +208,19 @@ def main(cam, disp):
 				timelapse_menu_draw.rectangle((83,48,106,56), fill=0x007000)
 				timelapse_menu_draw.text((84,47), text=timelapse_resolution_options[timelapse_resolution_index], fill=0x00c7ff if timelapse_menu_selected_item == 1 else 0xffffff)
 				rotated_screen = timelapse_menu_screen.rotate(180)
+				disp.LCD_ShowImage(rotated_screen, 0, 0)
+
+			elif current_menu_index == settings_menu_index:
+				if input_key == press_pin:
+					current_menu_index = 0
+				elif input_key == left_pin and exposure_mode_index > 0:
+					exposure_mode_index -= 1
+				elif input_key == right_pin and exposure_mode_index < 12:
+					exposure_mode_index += 1
+				settings_menu_draw.rectangle((70,31,128,40), fill=0xd89552)
+				settings_menu_draw.text((70,30), text=exposure_modes[exposure_mode_index], fill=0x00c7ff)
+				cam.exposure_mode = exposure_modes[exposure_mode_index]
+				rotated_screen = settings_menu_screen.rotate(180)
 				disp.LCD_ShowImage(rotated_screen, 0, 0)
 
 			time.sleep(0.1) #reduced framerate in menu
@@ -276,7 +307,7 @@ def main(cam, disp):
 		#check if internet connection is available and displey the cameras ip address
 		connection_status = subprocess.check_output("hostname -I", text=True, shell=True)[:13]
 		overlay_draw.text( (27,115), text= connection_status if connection_status != "" else "no connection", fill=0xffffff)
-		if time.time() - last_interaction_time > 10 and not timelapse_capture_flag:
+		if time.time() - last_interaction_time > 60 and not timelapse_capture_flag:
 			overlay_draw.text( (30,100), text="Power saving", fill=0xffffff)
 			time.sleep(0.5)
 		overlay = overlay.rotate(180)
